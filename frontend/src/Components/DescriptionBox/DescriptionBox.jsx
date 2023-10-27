3aeffac3f003778606bf2ecb1e259707436cda1c
Noufara Ahamed <113227922+NoufaraAhamed@users.noreply.github.com>
2023-10-27 13:59:46 +0400

    feat: add descriptionbox

--- frontend/src/Components/DescriptionBox/DescriptionBox.jsx
+++ frontend/src/Components/DescriptionBox/DescriptionBox.jsx
@@ -4,6 +4,16 @@ import './DescriptionBox.css'
 function DescriptionBox() {
   return (
     <div className='descriptionbox'>
+      <div className="descriptionbox-navigator">
+        <div className="descriptionbox-nav-box">Description</div>
+        <div className="descriptionbox-nav-box fade">Reviews (122)</div>
+      </div>
+      <div className="descriptionbox-description">
+        <p>
+        Casual shirt to wear out alert! Check out our new all-cotton flannel shirt! This shirt is made from 100% cotton, making it soft to the touch and gentle on the skin. The long-sleeve design is perfect for those cooler days or nights, while the button-up closure makes it easy to take on and off. The plaid design is perfect for any casual occasion, whether going to a barbecue or running errands. The shirt is made in Italy, so you know you’re getting a high-quality product.      
+        </p>
+        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Consequatur debitis amet cum vitae tempore et numquam quisquam eligendi eum laborum hic ex, ab cumque sunt itaque temporibus quam neque iure.</p>
+      </div>
       
     </div>
   )
---
+++ frontend/src/Components/RelatedProducts/RelatedProducts.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import './RelatedProducts.css'
+
+
+function RelatedProducts() {
+  return (
+    <div className='relatedproducts'>
+      <h1>Related Products</h1>
+      <hr />
+      <div className="relatedproducts-item">
+
+      </div>
+    </div>
+  )
+}
+
+export default RelatedProducts
